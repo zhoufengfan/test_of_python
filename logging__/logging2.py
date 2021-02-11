50eaa1b7b15d799d07262005e17d8050f815d569
zhoufengfan <zhfengfan@126.com>
2021-02-11 13:47:20 +0800

    add RichHandler() to logging2

--- logging__/logging2.py
+++ logging__/logging2.py
@@ -1,15 +1,20 @@
 import logging
-from logging__.foo import foo2
+# from logging__.foo import foo2
+from rich.logging import RichHandler
 
 if __name__ == '__main__':
+    use_rich_handler = True
     logging.basicConfig(filename='some_log.log',
                         filemode="w",
                         format='%(levelname)s\t%(message)s\t%(asctime)s\t%(pathname)s\tLine:%(lineno)d',
                         level=logging.DEBUG)
-    sh = logging.StreamHandler()
+    if use_rich_handler:
+        sh = RichHandler()
+    else:
+        sh = logging.StreamHandler()
     # use add addHandler to enable the logging print in the console.
     logging.getLogger('').addHandler(sh)
-    logging.debug('Debug message')
+    logging.debug("[bold red blink]debug message[/]", extra={"markup": True})
     logging.info('Info message')
     logging.warning('Warning message')
-    foo2()
+    # foo2()
