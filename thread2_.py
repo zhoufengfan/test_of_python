850a20ed4aef704ccc54c62cfe430c37762b9e7c
zhoufengfan <zhfengfan@126.com>
2021-05-05 15:42:39 +0800

    add thread2_

---
+++ thread2_.py
@@ -0,0 +1,28 @@
+import threading
+import time
+
+
+class Action(threading.Thread):
+    def __init__(self, name):
+        threading.Thread.__init__(self)
+        self.name = name
+
+    def run(self):
+        print("Thread {} start".format(self.name))
+        self.foo(self.name)
+        print("Thread {} finish".format(self.name))
+
+    def foo(self, thread_name):
+        for i in range(5):
+            time.sleep(0.2)
+            print(thread_name)
+
+
+if __name__ == '__main__':
+    thread1 = Action("work")
+    thread2 = Action("rest")
+
+    thread1.start()
+    thread2.start()
+    thread1.join()
+    thread2.join()
